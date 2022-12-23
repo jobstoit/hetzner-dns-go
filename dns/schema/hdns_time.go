64ad01265c7407c5360374a3f1d25545a7f41140
Jonas Badstübner <jonas.badstuebner@hetzner-cloud.de>
2022-12-23 14:01:05 +0100

    fix(HdnsTime): add 3rd possible format (#2)
    * fix(HdnsTime): add 3rd possible format
    
    * refactor(HdnsTime): unnest error handling
    
    * fix(HdnsTime): readd split
    
    * refactor(HdnsTime): check len before trim

--- dns/schema/hdns_time.go
+++ dns/schema/hdns_time.go
@@ -6,31 +6,40 @@ import (
 	"time"
 )
 
-// HdnsTime defines a wrapper for time.Time, to handle the DateTime format used in HCloud DNS API
+// HdnsTime defines a wrapper for time.Time, to handle the DateTime format(s) used in HCloud DNS API...
 type HdnsTime time.Time
 
-func (ht *HdnsTime) UnmarshalJSON(b []byte) (err error) {
-	hdns_time_layout := "2006-01-02 15:04:05 -0700 UTC"
+func (ht *HdnsTime) UnmarshalJSON(b []byte) error {
 
-	s := strings.Trim(string(b), "\"")
-
-	if len(s) == 0 {
+	if len(b) == 0 {
 		*ht = HdnsTime{}
 		return nil
 	}
 
-	t, err := time.Parse(hdns_time_layout, s)
-	if err != nil {
-		t2, err2 := time.Parse(time.RFC3339, s)
+	s := strings.Trim(string(b), "\"")
 
-		if err2 != nil {
-			return fmt.Errorf("Error while parsing date '%s' with default rfc3339 time layout and '%s': %s\n", s, hdns_time_layout, err)
-		}
-		t = t2
+	hdns_time_layout_1 := "2006-01-02 15:04:05 -0700 UTC"
+
+	if t, err := time.Parse(hdns_time_layout_1, s); err == nil {
+		*ht = HdnsTime(t)
+		return nil
 	}
 
-	*ht = HdnsTime(t)
-	return nil
+	// Real example API return value:
+	// [...] "verified": "2020-04-07 01:56:03.196438163 +0000 UTC m=+755.322810452", [...]
+	hdns_time_layout_2 := "2006-01-02 15:04:05.000000000 -0700 UTC" //... m=+755.322810452"
+
+	if t, err := time.Parse(hdns_time_layout_2, strings.Split(s, " m=+")[0]); err == nil {
+		*ht = HdnsTime(t)
+		return nil
+	}
+
+	if t, err := time.Parse(time.RFC3339, s); err == nil {
+		*ht = HdnsTime(t)
+		return nil
+	}
+
+	return fmt.Errorf("Error while parsing date '%s'\n", s)
 }
 
 func (ht HdnsTime) MarshalJSON() ([]byte, error) {
