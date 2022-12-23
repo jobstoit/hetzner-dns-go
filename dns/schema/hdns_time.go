a9b321a277b4b95724ee103fe95cce5757c812de
Jonas Badstübner <jonas.badstuebner@hetzner-cloud.de>
2022-12-23 13:46:55 +0100

    refactor(HdnsTime): unnest error handling

--- dns/schema/hdns_time.go
+++ dns/schema/hdns_time.go
@@ -10,10 +10,6 @@ import (
 type HdnsTime time.Time
 
 func (ht *HdnsTime) UnmarshalJSON(b []byte) error {
-	hdns_time_layout_1 := "2006-01-02 15:04:05 -0700 UTC"
-	// Real example API return value:
-	// [...] "verified": "2020-04-07 01:56:03.196438163 +0000 UTC m=+755.322810452", [...]
-	hdns_time_layout_2 := "2006-01-02 15:04:05.000000000 -0700 UTC" //... m=+755.322810452"
 
 	s := strings.Trim(string(b), "\"")
 
@@ -22,25 +18,28 @@ func (ht *HdnsTime) UnmarshalJSON(b []byte) error {
 		return nil
 	}
 
-	t, err_format1 := time.Parse(hdns_time_layout_1, s)
-	if err_format1 != nil {
+	hdns_time_layout_1 := "2006-01-02 15:04:05 -0700 UTC"
 
-		t_format2, err_format2 := time.Parse(hdns_time_layout_2, strings.Split(s, " m=+")[0])
-		t = t_format2
-
-		if err_format2 != nil {
-
-			time_rfc3339, err_rfc3339 := time.Parse(time.RFC3339, s)
-			t = time_rfc3339
-
-			if err_rfc3339 != nil {
-				return fmt.Errorf("Error while parsing date '%s' with\n;; '%s' (err: %s)\n;; '%s' (err: %s)\n;; default rfc3339 time layout (err: %s)\n", s, hdns_time_layout_1, err_format1, hdns_time_layout_2, err_format2, err_rfc3339)
-			}
-		}
+	if t, err := time.Parse(hdns_time_layout_1, s); err == nil {
+		*ht = HdnsTime(t)
+		return nil
 	}
 
-	*ht = HdnsTime(t)
-	return nil
+	// Real example API return value:
+	// [...] "verified": "2020-04-07 01:56:03.196438163 +0000 UTC m=+755.322810452", [...]
+	hdns_time_layout_2 := "2006-01-02 15:04:05.000000000 -0700 UTC" //... m=+755.322810452"
+
+	if t, err := time.Parse(hdns_time_layout_2, s); err == nil {
+		*ht = HdnsTime(t)
+		return nil
+	}
+
+	if t, err := time.Parse(time.RFC3339, s); err == nil {
+		*ht = HdnsTime(t)
+		return nil
+	}
+
+	return fmt.Errorf("Error while parsing date '%s'\n", s)
 }
 
 func (ht HdnsTime) MarshalJSON() ([]byte, error) {
